2aa16a7d0c4b874b6991167883a87046328a6202
krusnabalar <90931286+krusnabalar@users.noreply.github.com>
2023-12-01 04:16:56 +0530

    added 2023 files

---
+++ 2023/java/Day15.java
@@ -0,0 +1,15 @@
+/*
+
+
+*/
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Day15 {
+    public static void main(String... args) throws Exception {
+        List<String> allLines = Files.readAllLines(Paths.get("../testInput/day3.txt"));
+
+    }
+}
