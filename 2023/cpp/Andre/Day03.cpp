01fe6bc74f12ec926c03dc05b99ac3f91adcd6c3
Andre <mellowcoding@gmail.com>
2023-12-07 11:07:34 -0800

    day 3

--- 2023/cpp/Andre/Day03.cpp
+++ 2023/cpp/Andre/Day03.cpp
@@ -83,9 +83,91 @@ int part1() {
 }
 
 int part2() {
+    std::ifstream file("input/day03.txt");
+
+    if (!file.is_open()) {
+        std::cerr << "file did not open.\n";
+        return 1;
+    }
+
+    std::string line;
+
+    char c;
+    bool newNum = true;
+    std::vector<int> nums = {};
+    // locs to index in nums
+    std::unordered_map<std::tuple<int,int>, int> numLocations;
+    std::vector<std::tuple<int, int>> specialLocations;
+    int lineNumber = 0;
+    int lineLength;
+
+    while (std::getline(file, line)) {
+        lineLength = line.length();
+        for (int i = 0; i < lineLength; i++) {
+            c = line[i];
+            if (c == '.') {
+                newNum = true;
+            } else if (std::isdigit(c)) {
+                if (newNum) {
+                    nums.push_back(0);   
+                }
+                newNum = false;
+                nums.back() = 10 * nums.back() + (c - '0');
+                numLocations[std::tuple<int,int>(lineNumber, i)] = nums.size() - 1;
+            } else if (c == '*') {
+                newNum = true;
+                specialLocations.push_back(std::tuple<int, int>(lineNumber, i));
+            } else {
+                newNum = true;
+            }
+        }
+        newNum = true;
+        lineNumber++;
+    }
+
+    int sum = 0;
+
+
+    for (std::tuple<int, int> loc : specialLocations) {
+        int row = std::get<0>(loc);
+        int col = std::get<1>(loc);
+        int hits = 0;
+        std::unordered_map<int, bool> hitSet;
+
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                // skip center
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                // skip edges
+                if (row + i >= 0 && row + i < lineLength && col + j >= 0 && col + j < lineLength) {
+                    // if neighbot exists
+                    if (auto numLoc = numLocations.find(std::tuple<int, int>(row+i, col+j)); numLoc != numLocations.end()) {
+                        // if not already hit
+                        if (auto hitNum = hitSet.find(numLoc->second); hitNum == hitSet.end()) {
+                            hitSet[numLoc->second] = true;
+                            hits++;
+                        }
+                    } 
+                }
+            
+            }
+        }
+        if (hits == 2) {
+            int mult = 1;
+            for (auto [key, value] : hitSet) {
+                mult *= nums[key];
+            }
+            sum += mult;
+        }
+        
+    }
+
+    std::cout << sum << std::endl;
     return 0;
 }
 
 int main() {
-    return part1();
+    return part2();
 }
