e280b07a1bc7d4e7950ba8ec799a4d37ca0a35a7
krusnabalar <90931286+krusnabalar@users.noreply.github.com>
2023-12-28 01:46:05 +0530

    krusna day 16 and 25

--- 2023/java/krusna/day16/Day16.java
+++ 2023/java/krusna/day16/Day16.java
@@ -5,8 +5,195 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Day16 {
-    public static void main(String... args) throws Exception {
-        List<String> allLines = Files.readAllLines(Paths.get("./day16Test.txt"));
+    List<String> allLines = new ArrayList<>();
+    char[][] grid;
+    // WSEN
+    int directionInX[] = new int[] { -1, 0, 1, 0 };
+    int directionInY[] = new int[] { 0, 1, 0, -1 };
 
+    public class Incoming {
+        int[] tile;
+        int direction;
+
+        private Incoming(int[] tile, int direction) {
+            this.tile = tile;
+            this.direction = direction;
+        }
+    }
+
+    void setGrid() {
+        int rowNum = 0;
+        grid = new char[allLines.size()][];
+        for (String line : allLines) {
+            grid[rowNum++] = line.toCharArray();
+        }
+    }
+
+    int countVisited(int[][] visited) {
+        int res = 0;
+        for (int i = 0; i < visited.length; i++) {
+            for (int j = 0; j < visited.length; j++) {
+                res += visited[i][j] > 0 ? 1 : 0;
+            }
+        }
+        return res;
+    }
+
+    boolean isValidTile(int[] tile) {
+        return !(tile[0] < 0 || tile[0] >= grid.length || tile[1] < 0 || tile[1] >= grid[0].length);
+    }
+
+    List<Incoming> getOutgoing(Incoming incoming) {
+        List<Incoming> outgoing = new ArrayList<>();
+        int[] pos = incoming.tile;
+        int dir = incoming.direction;
+        char tileType = grid[pos[0]][pos[1]];
+        int[] nextTile, otherNextTile;
+        int nextDir = 0;
+        switch (tileType) {
+            case '.':
+                nextTile = new int[] { pos[0] + directionInY[dir], pos[1] + directionInX[dir] };
+                if (isValidTile(nextTile)) {
+                    outgoing.add(new Incoming(nextTile, dir));
+                }
+                break;
+            case '-':
+                if (dir == 0 || dir == 2) {
+                    nextTile = new int[] { pos[0] + directionInY[dir], pos[1] + directionInX[dir] };
+                    if (isValidTile(nextTile)) {
+                        outgoing.add(new Incoming(nextTile, dir));
+                    }
+                } else {
+                    int dir1 = 0, dir2 = 2;
+                    nextTile = new int[] { pos[0] + directionInY[dir1], pos[1] + directionInX[dir1] };
+                    otherNextTile = new int[] { pos[0] + directionInY[dir2], pos[1] + directionInX[dir2] };
+                    if (isValidTile(nextTile))
+                        outgoing.add(new Incoming(nextTile, dir1));
+                    if (isValidTile(otherNextTile)) {
+                        outgoing.add(new Incoming(otherNextTile, dir2));
+                    }
+                }
+                break;
+            case '|':
+                if (dir == 1 || dir == 3) {
+                    nextTile = new int[] { pos[0] + directionInY[dir], pos[1] + directionInX[dir] };
+                    if (isValidTile(nextTile)) {
+                        outgoing.add(new Incoming(nextTile, dir));
+                    }
+                } else {
+                    int dir1 = 1, dir2 = 3;
+                    nextTile = new int[] { pos[0] + directionInY[dir1], pos[1] + directionInX[dir1] };
+                    otherNextTile = new int[] { pos[0] + directionInY[dir2], pos[1] + directionInX[dir2] };
+                    if (isValidTile(nextTile))
+                        outgoing.add(new Incoming(nextTile, dir1));
+                    if (isValidTile(otherNextTile))
+                        outgoing.add(new Incoming(otherNextTile, dir2));
+                }
+                break;
+            case '/':
+                if (dir == 0) {
+                    nextDir = 1;
+                } else if (dir == 1) {
+                    nextDir = 0;
+                } else if (dir == 2) {
+                    nextDir = 3;
+                } else {
+                    nextDir = 2;
+                }
+                nextTile = new int[] { pos[0] + directionInY[nextDir], pos[1] + directionInX[nextDir] };
+                if (isValidTile(nextTile)) {
+                    outgoing.add(new Incoming(nextTile, nextDir));
+                }
+                break;
+            case '\\':
+                if (dir == 0) {
+                    nextDir = 3;
+                } else if (dir == 1) {
+                    nextDir = 2;
+                } else if (dir == 2) {
+                    nextDir = 1;
+                } else {
+                    nextDir = 0;
+                }
+                nextTile = new int[] { pos[0] + directionInY[nextDir], pos[1] + directionInX[nextDir] };
+                if (isValidTile(nextTile)) {
+                    outgoing.add(new Incoming(nextTile, nextDir));
+                }
+                break;
+            default:
+        }
+        return outgoing;
+    }
+
+    int traverseGrid(Incoming start) {
+        int[][] visited = new int[allLines.size()][allLines.get(0).length()];
+        Map<List<Integer>, Set<Integer>> checked = new HashMap<>();
+        Queue<Incoming> queue = new LinkedList<>();
+        queue.add(start);
+        while (!queue.isEmpty()) {
+            Incoming currTile = queue.poll();
+            if (checked.containsKey(new ArrayList<>(Arrays.asList(currTile.tile[0], currTile.tile[1])))) {
+                if (checked.get(new ArrayList<>(Arrays.asList(currTile.tile[0], currTile.tile[1])))
+                        .contains(currTile.direction)) {
+                    continue;
+                }
+            }
+            visited[currTile.tile[0]][currTile.tile[1]] = 1;
+            List<Incoming> outgoing = getOutgoing(currTile);
+            Set<Integer> setOfDirs = checked.getOrDefault(currTile.tile, new HashSet<>());
+            setOfDirs.add(currTile.direction);
+            checked.put(new ArrayList<>(Arrays.asList(currTile.tile[0], currTile.tile[1])), setOfDirs);
+            for (Incoming o : outgoing) {
+                queue.add(o);
+            }
+        }
+
+        return countVisited(visited);
+    }
+
+    int getMaxVisited() {
+        int maxRes = -1;
+        // top left corner
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { 0, 0 }, 1)), maxRes);
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { 0, 0 }, 2)), maxRes);
+        // bottom left corner
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { 0, grid.length - 1 }, 2)), maxRes);
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { 0, grid.length - 1 }, 3)), maxRes);
+        // top right corner
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { grid[0].length - 1, 0 }, 0)), maxRes);
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { grid[0].length - 1, 0 }, 1)), maxRes);
+        // bottom right corner
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { grid[0].length - 1, grid.length - 1 }, 0)), maxRes);
+        maxRes = Math.max(traverseGrid(new Incoming(new int[] { grid[0].length - 1, grid.length - 1 }, 3)), maxRes);
+
+        for (int i = 1; i < grid.length - 1; i++) {
+            maxRes = Math.max(traverseGrid(new Incoming(new int[] { i, 0 }, 2)), maxRes);
+            maxRes = Math.max(traverseGrid(new Incoming(new int[] { i, grid.length - 1 }, 0)), maxRes);
+        }
+
+        for (int i = 1; i < grid[0].length - 1; i++) {
+            maxRes = Math.max(traverseGrid(new Incoming(new int[] { 0, i }, 1)), maxRes);
+            maxRes = Math.max(traverseGrid(new Incoming(new int[] { grid[0].length - 1, i }, 3)), maxRes);
+        }
+
+        return maxRes;
+    }
+
+    void part1() {
+        int res = traverseGrid(new Incoming(new int[] { 0, 0 }, 2));
+        System.out.printf("Part 1: %d\n", res);
+    }
+
+    void part2() {
+        int maxRes = getMaxVisited();
+        System.out.printf("Part 2: %d\n", maxRes);
+    }
+
+    public static void main(String... args) throws Exception {
+        Day16 day16 = new Day16();
+        day16.allLines = Files.readAllLines(Paths.get("./day16.txt"));
+        day16.setGrid();
+        day16.part1();
+        day16.part2();
     }
 }
--- 2023/java/krusna/day25/Day25.java
+++ 2023/java/krusna/day25/Day25.java
@@ -1,16 +1,85 @@
 package krusna.day25;
-/*
-
-
-*/
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class Day25 {
-    public static void main(String... args) throws Exception {
-        List<String> allLines = Files.readAllLines(Paths.get("../day25Test.txt"));
+    List<String> allLines;
+    List<String[]> edgesOriginal = new ArrayList<>();
 
+    private void buildGraph() {
+        for (String line : allLines) {
+            String nodes[] = line.replace(":", "").split(" ");
+            String src = nodes[0];
+            for (int i = 1; i < nodes.length; i++) {
+                edgesOriginal.add(new String[] { src, nodes[i] });
+            }
+        }
+    }
+
+    private int kragersMinCut() {
+        List<String[]> edges = new ArrayList<>();
+
+        // copy edges
+        for (String[] edge : edgesOriginal) {
+            edges.add(new String[] { edge[0], edge[1] });
+        }
+
+        Random rand = new Random();
+        // contract edges until only 3 edges remain (min-cut size)
+        while (edges.size() > 3) {
+            // contract edge
+            int randomEdge = rand.nextInt(edges.size());
+            String[] contractingNodes = edges.get(randomEdge);
+            String newVertex = contractingNodes[0] + contractingNodes[1];
+            edges.remove(randomEdge);
+
+            // replace vertices from original vertices with contracted vertex
+            for (int i = 0; i < edges.size(); i++) {
+                String[] edge = edges.get(i);
+                if (contractingNodes[0].equals(edge[0]) || contractingNodes[1].equals(edge[0])) {
+                    edge[0] = newVertex;
+                }
+                if (contractingNodes[0].equals(edge[1]) || contractingNodes[1].equals(edge[1])) {
+                    edge[1] = newVertex;
+                }
+                if (edge[0].equals(edge[1])) {
+                    edges.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        // check if there are 2 unique vertices
+        Set<String> uniqueNodes = new HashSet<>();
+        for (String[] edge : edges) {
+            uniqueNodes.add(edge[0]);
+            uniqueNodes.add(edge[1]);
+        }
+        if (uniqueNodes.size() != 2) {
+            return -1;
+        }
+
+        // multiply size of the 2 contracted vertices
+        int key = edges.get(0)[0].length();
+        int values = edges.get(0)[1].length();
+        return (key / 3) * (values / 3);
+    }
+
+    private void part1() {
+        int res = -1;
+        // repeat until the monte carlo method gives a valid output
+        while (res < 0) {
+            res = kragersMinCut();
+        }
+        System.out.printf("Part 1: %d\n", res);
+    }
+
+    public static void main(String... args) throws Exception {
+        Day25 day25 = new Day25();
+        day25.allLines = Files.readAllLines(Paths.get("./day25.txt"));
+        day25.buildGraph();
+        day25.part1();
     }
 }
