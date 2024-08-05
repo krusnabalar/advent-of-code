325063d5ab4bd1db9797dae17e1d6a03e1c71c01
krusnabalar <90931286+krusnabalar@users.noreply.github.com>
2024-08-05 01:48:28 -0700

    Day24Pt2 2023 Java

--- 2023/java/krusna/day24/Day24.java
+++ 2023/java/krusna/day24/Day24.java
@@ -7,7 +7,8 @@ import java.util.*;
 public class Day24 {
     List<String> allLines;
     List<Hailstone> hailstones = new ArrayList<>();
-    long minVal, maxVal;
+    long minVal = 200000000000000L;
+    long maxVal = 400000000000000L;
 
     void setupHailstones() {
         for (String line : allLines) {
@@ -39,24 +40,18 @@ public class Day24 {
     }
 
     boolean intersect2D(Hailstone h1, Hailstone h2) {
-        // Paramteric:
-        //      x = x_0 + v_x * t
-        //      y = y_0 + v_y * t
-        // Standard:
-        //      (x - x_0) / v_x = (y - y_0) / v_y
-        //      v_y * x - v_x * y   = v_y * x_0 - v_x * y_0
-        //      a_1 * x + b_1 * y   = a_2 * x   + b_2 * y
+        // vy * x - vx * y  = vy * x0 - vx * y0
         double a1 = h1.v.yVel, a2 = h2.v.yVel;
         double b1 = -h1.v.xVel, b2 = -h2.v.xVel;
         double det = a1 * b2 - a2 * b1;
+        if (det == 0) {
+            return false;
+        }
         double c1 = a1 * h1.startPos.xPos + b1 * h1.startPos.yPos;
         double c2 = a2 * h2.startPos.xPos + b2 * h2.startPos.yPos;
         double x = (b2 * c1 - b1 * c2) / det;
         double y = (a1 * c2 - a2 * c1) / det;
 
-        if (Math.abs(det) < 1e-9) {
-            return false;
-        }
         if (x < minVal || x > maxVal || y < minVal || y > maxVal) {
             return false;
         }
@@ -66,14 +61,91 @@ public class Day24 {
         return t1 >= 0 && t2 >= 0;
     }
 
+    long[] intersectingRock() {
+        double[][] matrix = new double[6][6];
+        double[] constants = new double[6];
+        for (int i = 0; i < 2; i++) {
+            Hailstone h1 = hailstones.get(i);
+            Hailstone h2 = hailstones.get(i + 1);
+            long x1_0 = h1.startPos.xPos, y1_0 = h1.startPos.yPos, z1_0 = h1.startPos.zPos;
+            long x2_0 = h2.startPos.xPos, y2_0 = h2.startPos.yPos, z2_0 = h2.startPos.zPos;
+            long vx_1 = h1.v.xVel, vy_1 = h1.v.yVel, vz_1 = h1.v.zVel;
+            long vx_2 = h2.v.xVel, vy_2 = h2.v.yVel, vz_2 = h2.v.zVel;
+
+            // Equation 1: (Ry - H1y)(Rvx - H1vx) = (Rx - H1x)(Rvy - H1vy)
+            // Equation 2: (Rz - H1z)(Rvx - H1vx) = (Rx - H1x)(Rvz - H1vz)
+            // Equation 3: (Rz - H1z)(Rvy - H1vy) = (Ry - H1y)(Rvz - H1vz)
+
+            // Eq1
+            matrix[i*3][0] = vy_2 - vy_1;
+            matrix[i*3][1] = vx_1 - vx_2;
+            matrix[i*3][3] = y1_0 - y2_0;
+            matrix[i*3][4] = x2_0 - x1_0;
+            constants[i*3] = y1_0 * vx_1 - x1_0 * vy_1 - y2_0 * vx_2 + x2_0 * vy_2;
+
+            // Eq2
+            matrix[i*3+1][0] = vz_2 - vz_1;
+            matrix[i*3+1][2] = vx_1 - vx_2;
+            matrix[i*3+1][3] = z1_0 - z2_0;
+            matrix[i*3+1][5] = x2_0 - x1_0;
+            constants[i*3+1] = z1_0 * vx_1 - x1_0 * vz_1 - z2_0 * vx_2 + x2_0 * vz_2;
+
+            // Eq3
+            matrix[i*3+2][1] = vz_2 - vz_1;
+            matrix[i*3+2][2] = vy_1 - vy_2;
+            matrix[i*3+2][4] = z1_0 - z2_0;
+            matrix[i*3+2][5] = y2_0 - y1_0;
+            constants[i*3+2] = z1_0 * vy_1 - y1_0 * vz_1 - z2_0 * vy_2 + y2_0 * vz_2;
+        }
+
+        return gaussianElimination(matrix, constants);
+    }
+
+    private long[] gaussianElimination(double[][] matrix, double[] constants) {
+        int n = constants.length;
+        for (int i = 0; i < n; i++) {
+            int maxElement = i;
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(matrix[j][i]) > Math.abs(matrix[maxElement][i])) {
+                    maxElement = j;
+                }
+            }
+
+            double[] temp = matrix[i];
+            matrix[i] = matrix[maxElement];
+            matrix[maxElement] = temp;
+            double t = constants[i];
+            constants[i] = constants[maxElement];
+            constants[maxElement] = t;
+
+            for (int j = i + 1; j < n; j++) {
+                double factor = matrix[j][i] / matrix[i][i];
+                constants[j] -= factor * constants[i];
+                for (int k = i; k < n; k++) {
+                    matrix[j][k] -= factor * matrix[i][k];
+                }
+            }
+        }
+
+        long[] result = new long[n];
+        for (int i = n - 1; i >= 0; i--) {
+            double sum = 0.0;
+            for (int j = i + 1; j < n; j++) {
+                sum += matrix[i][j] * result[j];
+            }
+            result[i] = (long) ((constants[i] - sum) / matrix[i][i]);
+        }
+        return result;
+    }
+
     public static void main(String... args) throws Exception {
         Day24 day24 = new Day24();
-        day24.minVal = 200000000000000L;
-        day24.maxVal = 400000000000000L;
         day24.allLines = Files.readAllLines(Paths.get("./2023/java/krusna/day24/day24.txt"));
         day24.setupHailstones();
-        System.out.printf("Part 1: %d\n", day24.intersectingHailstones());
+        long[] rockPosition = day24.intersectingRock();
 
+        System.out.printf("Part 1: %d\n", day24.intersectingHailstones());
+        System.out.printf("Part 2: %d\n", rockPosition[0] + rockPosition[1] + rockPosition[2]);
     }
 }
 
